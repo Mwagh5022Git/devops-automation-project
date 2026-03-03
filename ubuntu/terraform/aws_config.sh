7e95dc22aa2a273ac9e722e5243f5595b9daef51
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-03 21:48:49 +0530

    Update aws_config.sh

--- ubuntu/terraform/aws_config.sh
+++ ubuntu/terraform/aws_config.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+echo "========== AWS LOGIN =========="
+read -p "Enter AWS Access Key: " ACCESS_KEY
+read -s -p "Enter AWS Secret Key: " SECRET_KEY
+echo
+read -p "Enter AWS Region: " REGION
+
+aws configure set aws_access_key_id $ACCESS_KEY
+aws configure set aws_secret_access_key $SECRET_KEY
+aws configure set region $REGION
+
+echo "AWS Configured Successfully"
+
+echo "========== Terraform INIT =========="
+terraform init
+
+echo "========== Terraform PLAN =========="
+terraform plan
+
+echo "========== Terraform APPLY =========="
+terraform apply -auto-approve
+
+echo "========== SERVICES CREATED =========="
+terraform output
+
+echo "========== PROCESS COMPLETED =========="
