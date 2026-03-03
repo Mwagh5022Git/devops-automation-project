9d7edd4abbd20cebc5dc7e9883bdf2230ac6ecf1
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-03 22:34:44 +0530

    Create cloudfront.tf

---
+++ ubuntu/terraform/cloudfront.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = "s3origin"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3origin"
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
