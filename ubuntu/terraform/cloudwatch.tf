af5a3f45fadd338a23337c9051d5ea044ab130e4
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-04 00:02:05 +0530

    Update cloudwatch.tf

--- ubuntu/terraform/cloudwatch.tf
+++ ubuntu/terraform/cloudwatch.tf
@@ -2,3 +2,10 @@ resource "aws_cloudwatch_log_group" "log" {
   name = "/devops/app"
   retention_in_days = 7
 }
+forwarded_values {
+  query_string = false
+
+  cookies {
+    forward = "none"
+  }
+}
