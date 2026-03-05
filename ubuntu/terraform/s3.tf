f8f39baba7965d1ce51dc79a965ca16bbde054fa
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-05 17:46:22 +0530

    Update s3.tf

--- ubuntu/terraform/s3.tf
+++ ubuntu/terraform/s3.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_s3_bucket" "bucket" {
   bucket = "manish-devops-bucket-12345"
 }
@@ -6,5 +7,13 @@ resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+*/
+
+resource "aws_s3_bucket" "site" {
+  bucket = "mwagh-devops-project-bucket-2026"
+  tags = {
+    Name = "DevOpsProjectBucket"
   }
 }
