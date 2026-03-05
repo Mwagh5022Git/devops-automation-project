01dc315bee863e366398c42328415a33fa1d7d2a
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-05 18:20:36 +0530

    Update vpc.tf

--- ubuntu/terraform/vpc.tf
+++ ubuntu/terraform/vpc.tf
@@ -6,7 +6,6 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
 }
 resource "aws_subnet" "public_1" {
